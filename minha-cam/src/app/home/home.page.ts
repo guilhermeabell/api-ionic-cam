00a6d720d2439f1289b58d0d1040a8ad6870d7c5
Isabela <isa.gh.monteiro@gmail.com>
2020-10-28 09:53:00 -0300

    teste-api-cam

---
+++ minha-cam/src/app/home/home.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Plugins, CameraResultType, CameraSource} from '@capacitor/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  photo: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  async tirarfoto() {
+    const image = await Plugins.Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+    
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
+   /* var formData = new FormData();
+    formData.append("image", this.file);
+    this.httpClient.post<any>('http://127.0.0.1:5000/prediction', formData); */
+  }
+}
